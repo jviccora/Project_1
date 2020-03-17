38fccb1a3d65cc7c4860119bb2f6839a147ebf6f
beahaus <54328805+beahaus@users.noreply.github.com>
2020-03-16 21:33:47 -0400

    added basic ajax request from documentation

--- stock.js
+++ stock.js
@@ -1,8 +1,21 @@
-var stockTickerApiKey = j1OSOs2MNhy0kx3NISNHhhm2GrKrKhuSu5nVtG8PlwImQf4BThjeyMVeWPqJ;
 
-var queryURL = 
+// AJAX and queryURL pulled from World Trading Data documentation
+var url = new URL(
+    "https://api.worldtradingdata.com/api/v1/stock"
+);
 
-// AJAX calls
+let params = {
+    "symbol": "SNAP,TWTR,VOD.L",
+    "api_token": "j1OSOs2MNhy0kx3NISNHhhm2GrKrKhuSu5nVtG8PlwImQf4BThjeyMVeWPqJ",
+};
+Object.keys(params)
+    .forEach(key => url.searchParams.append(key, params[key]));
+
+fetch(url, {
+    method: "GET",
+})
+    .then(response => response.json())
+    .then(json => console.log(json));
 
 // pull appopriate data
 
