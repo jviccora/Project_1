46a39be21a440974639328239b10cdab6bac77a0
beahaus <54328805+beahaus@users.noreply.github.com>
2020-03-18 20:32:38 -0400

    index.html formatting for stock div

--- stock.js
+++ stock.js
@@ -1,22 +1,38 @@
+// variable declaration
+// var userInput = document.querySelector("#stock-input").value;
+var stockBtn = document.querySelector("#stock-search");
+var apiKey = "j1OSOs2MNhy0kx3NISNHhhm2GrKrKhuSu5nVtG8PlwImQf4BThjeyMVeWPqJ";
 
-// AJAX and queryURL pulled from World Trading Data documentation
-var url = new URL(
+// AJAX and queryURL from api documentation at https://www.worldtradingdata.com/documentation?javascript#stock-and-index-real-time
+
+ function stockSearch() {
+    
+const url = new URL(
     "https://api.worldtradingdata.com/api/v1/stock"
 );
-
+var userInput = document.querySelector("#stock-input").value;
 let params = {
-    "symbol": "SNAP",
-    "api_token": "j1OSOs2MNhy0kx3NISNHhhm2GrKrKhuSu5nVtG8PlwImQf4BThjeyMVeWPqJ",
+     "symbol": userInput,
+    //"symbol": "SNAP",
+    "api_token": apiKey,
 };
 Object.keys(params)
     .forEach(key => url.searchParams.append(key, params[key]));
 
-fetch(url, {
-    method: "GET",
-})
+fetch(url)
     .then(response => response.json())
-    .then(json => console.log(json));
-
-// pull appopriate data
+    .then(json => {
+        var tr = document.createElement("<tr>");
+        var td = document.createElement("<td>");
+        var stockSymbol = json.data[0].symbol;
+        document.querySelector("#td1").innerHTML = stockSymbol;
+        var stockName = json.data[0].name;
+        document.querySelector("#td2").innerHTML = stockName;
+        var stock52high = json.data[0].52_week_high;
+        console.log(json);
+        
+    });
+};
+stockBtn.addEventListener("click", stockSearch);
 
 // display appropriate data in table
