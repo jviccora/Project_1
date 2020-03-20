28da410a875ebc4373292d9f270861549d0992ed
Shaminder Singh <spsingh94.ss@gmail.com>
2020-03-20 16:05:27 -0400

    added coin ticker

--- bitcoin.js
+++ bitcoin.js
@@ -1,1 +1,9 @@
-var url = ""
+var apiKey = "21191d6105msh33bc7e64aa0f8d2p1ce9eajsn04ebeb9562c7";
+var apiUrl = "https://bravenewcoin-v1.p.rapidapi.com/prices?coin=btc&appid=21191d6105msh33bc7e64aa0f8d2p1ce9eajsn04ebeb9562c7";
+
+$.ajax({
+    url: apiUrl,
+    type: "GET"
+}).then(function (response) {
+    console.log(response);
+});
